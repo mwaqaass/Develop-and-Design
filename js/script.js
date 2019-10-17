9c2da5a9dc51385fe44d978a58b2a6c522d4f88c
Waqas <33598165+mwaqaass@users.noreply.github.com>
2019-10-18 01:09:51 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    $("#btn-contact").mouseover(function() {
+      $(".btn").toggleClass("animated");
+    });
+  });
+
+$(document).ready(function() {
+    $("#head-button").mouseover(function() {
+      $(".btn").toggleClass("animated");
+    });
+  });
+
+$(document).ready(function() {
+    $(".nav-item").mouseover(function() {
+      $("#nav-home").toggleClass("animated");
+    });
+  });
+
+$(document).ready(function() {
+    $(".nav-item2").mouseover(function() {
+      $("#nav-home2").toggleClass("animated");
+    });
+  });
+
+$(document).ready(function() {
+    $(".nav-item3").mouseover(function() {
+      $("#nav-home3").toggleClass("animated");
+    });
+  });
+
+$(document).ready(function() {
+    $(".nav-item4").mouseover(function() {
+      $("#nav-home4").toggleClass("animated");
+    });
+  });
+
+$(document).ready(function() {
+    $(".nav-item5").mouseover(function() {
+      $("#nav-home5").toggleClass("animated");
+    });
+  });
+
+$(document).ready(function() {
+    $(".nav-item6").mouseover(function() {
+      $("#nav-home6").toggleClass("animated");
+    });
+  });
+
+$(document).ready(function() {
+    $(".nav-item7").mouseover(function() {
+      $("#nav-home7").toggleClass("animated");
+    });
+  });
